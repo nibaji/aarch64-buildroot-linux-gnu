267a61bcb94767ddab7d198ffe47bf2f9277ef57
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2020-04-27 23:14:08 +0530

    gcc9: 2020.02.1
    https://git.busybox.net/buildroot/tag/?h=2020.02.1

---
+++ lib/gcc/aarch64-buildroot-linux-gnu/9.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/hyper/nibaji/buildroot-2020.02.1/output/host --sysconfdir=/home/hyper/nibaji/buildroot-2020.02.1/output/host/etc --enable-static --target=aarch64-buildroot-linux-gnu --with-sysroot=/home/hyper/nibaji/buildroot-2020.02.1/output/host/aarch64-buildroot-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/hyper/nibaji/buildroot-2020.02.1/output/host --with-mpc=/home/hyper/nibaji/buildroot-2020.02.1/output/host --with-mpfr=/home/hyper/nibaji/buildroot-2020.02.1/output/host --with-pkgversion='Buildroot 2020.02.1' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --enable-plugins --enable-lto --enable-threads --with-isl=/home/hyper/nibaji/buildroot-2020.02.1/output/host --with-abi=lp64 --with-cpu=cortex-a75.cortex-a55 --enable-languages=c,c++,d,fortran --with-build-time-tools=/home/hyper/nibaji/buildroot-2020.02.1/output/host/aarch64-buildroot-linux-gnu/bin --enable-shared --enable-libgomp march=armv8.2-a+crc mcpu=kryo300 mtune=a75.a55";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a75.cortex-a55" } };
